c4c94a289e53b5f333d1bee8e295333a7b33a830
archana <archanavadde88@gmail.com>
2023-03-22 14:18:21 +0000

    cmt

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-terraform {
-  
-  backend "s3" {
-    bucket = "archanavadde98765"
-    key = "archanavaddeobject.tfstate"
-    region = "us-east-1"
-    
-  }
-}
+
 provider "aws" {
     region = "us-east-1"
 }
