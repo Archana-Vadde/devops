e7501720e8135b0aceb323774a9e1fa74067d7a0
archana <archanavadde88@gmail.com>
2023-03-24 16:43:22 +0000

    kkk

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@
 provider "aws" {
     region = "us-east-1"
 }
+# backend data for terraform
+terraform{
+    backend "s3" {
+        bucket = "pipeline-artifacts-demmmmo"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        prompt = true
+    }
+}
+
+
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 
-# resource "aws_vpc" "name" {
-#     cidr_block = "10.0.0.0/16"
-#     tags = {
-#       "name" = "testt"
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "name" = "testt"
 
-#     }
+    }
   
-# }
+}
