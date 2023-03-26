d7dd413bfb4bbfa2253d6d26b56e251051db8ce4
archna94 <126779485+archna94@users.noreply.github.com>
2023-03-26 18:18:37 +0100

    deleting_file

--- terraform-destroy.sh
+++
@@ -1,8 +0,0 @@
-# - name: Prompt for confirmation
-#   run: |
-#     if [[ "${{ inputs.confirm_destroy }}" == "true" ]]; then
-#       terraform destroy --auto-approve
-#     else
-#       echo "Aborted."
-#     fi
-  
